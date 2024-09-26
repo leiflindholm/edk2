dbe7f6afe1dd3641018adcea462fd0df353dafc8
Leif Lindholm <quic_llindhol@quicinc.com>
2024-09-26 12:03:37 +0100

    FdtGetHeader/FdtTotalSize

--- MdePkg/Include/Library/FdtLib.h
+++ MdePkg/Include/Library/FdtLib.h
@@ -155,6 +155,10 @@ typedef struct {
   CHAR8     Data[];
 } FDT_PROPERTY;
 
+#define FdtGetHeader(Fdt, Field) \
+  (Fdt32ToCpu (((const FDT_HEADER *)(Fdt))->Field))
+#define FdtTotalSize(Fdt) (FdtGetHeader ((Fdt), TotalSize))
+
 /**
   Convert UINT16 data of the FDT blob to little-endian
 
