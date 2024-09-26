5baa4258fd9ad919450c82326abfe551ccf3f638
Leif Lindholm <quic_llindhol@quicinc.com>
2024-09-26 12:18:54 +0100

    FdtSetPropU64

--- MdePkg/Library/BaseFdtLib/FdtLib.c
+++ MdePkg/Library/BaseFdtLib/FdtLib.c
@@ -467,6 +467,32 @@ FdtSetProp (
 }
 
 /**
+  Set a property to a 64-bit integer.
+
+  @param[in] Fdt            The pointer to FDT blob.
+  @param[in] NodeOffset     The offset to the node offset which want to add in.
+  @param[in] Name           The name to name the property.
+  @param[in] Value          The value (big-endian) to the property value.
+
+  @return  Zero for successfully, otherwise failed.
+
+ **/
+INT32
+EFIAPI
+FdtSetPropU64 (
+  IN VOID         *Fdt,
+  IN INT32        NodeOffset,
+  IN CONST CHAR8  *Name,
+  IN UINT64       Value
+  )
+{
+  UINT64 Tmp;
+  Tmp = cpu_to_fdt64 (Value);
+
+  return fdt_setprop (Fdt, NodeOffset, Name, &Tmp, sizeof(Tmp));
+}
+
+/**
   Delete a property.
 
   This function will delete data from the blob, and will therefore
